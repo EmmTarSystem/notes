321de8bdbbe878c7867b7fdef9a011b2e09764d4
EmmTarSystem <manu974.training@gmail.com>
2023-10-15 19:26:53 +0200

    0.7.0

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Entrez ici les instructions pour bien débuter avec votre projet...
 
 ## Versions
 
-**Dernière version stable :** 0.5.0
-**Dernière version :** 0.6.0
+**Dernière version stable :** 0.7.0
+**Dernière version :** 0.7.0
 
 
 ## Auteurs
--- scripts/print.js
+++ scripts/print.js
@@ -1,14 +1,28 @@
 function onClickPrint() {
     // Récupère le contenu de la div
-    let contentToPrint = document.getElementById('divNoteView').innerHTML;
+    let contentToPrint = document.getElementById('printArea').innerHTML;
 
     // Creation fenetre et insertion des éléments à imprimer
     let pageToPrint = window.open('', '', 'width=600,height=600');
     pageToPrint.document.open();
-    pageToPrint.document.write('<html><head><title>Impression</title></head><body>');
+    pageToPrint.document.write('<html><head><title>Tâches</title>');
+    // Style
+    pageToPrint.document.write('<style>');
+    pageToPrint.document.write('table, th, td{width: 800px;text-align: center;}');
+    pageToPrint.document.write('tr.viewNoteRowTitle p{padding-top:30px;font-size: 3ch;font-weight: bold;height:100px;}');
+    pageToPrint.document.write('tr.tableViewNoteRowDetail p{text-align: left;font-size: 2.8ch;text-align: justify;height:300px;}');
+    pageToPrint.document.write('tr.tableViewNoteRowStep{height: 500px;vertical-align: top;}');
+    pageToPrint.document.write('tr.tableViewNoteRowStep li{text-align: left;}');
+    pageToPrint.document.write('</style>');
+    // Fin style
+    pageToPrint.document.write('</head><body>');
+    // Fin du HEAD
     pageToPrint.document.write(contentToPrint);
     pageToPrint.document.write('</body></html>');
     pageToPrint.document.close();
     pageToPrint.print();
     pageToPrint.close();
 }
+
+
+
